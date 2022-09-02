c04e7b1a5dfcae102a3bf5035dcecaba165dbd69
Vsnoy <59299768+Vsnoy@users.noreply.github.com>
2022-09-02 04:13:30 +0000

    Deploying to gh-pages from  @ 87e7481f4e454db1102e7ae15829de98052ee37f 🚀

---
+++ assets/git 常用命令.html.5f4e87a9.js
@@ -0,0 +1,14 @@
+import{_ as e,o as i,c as n,d as a}from"./app.555da4ec.js";const d={},s=a(`<h1 id="git-\u5E38\u7528\u547D\u4EE4" tabindex="-1"><a class="header-anchor" href="#git-\u5E38\u7528\u547D\u4EE4" aria-hidden="true">#</a> git \u5E38\u7528\u547D\u4EE4</h1><div class="custom-container tip"><p class="custom-container-title">TIP</p><p>\u540E\u9762\u7528\u5230\u5C31\u8BB0\u5F55\u4E0B\uFF0C\u6301\u7EED\u66F4\u65B0\u3002</p></div><h2 id="\u914D\u7F6E" tabindex="-1"><a class="header-anchor" href="#\u914D\u7F6E" aria-hidden="true">#</a> \u914D\u7F6E</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>// \u67E5\u770B\u914D\u7F6E
+git config --global --list
+
+// \u6DFB\u52A0\u914D\u7F6E
+git config --global xxx xxx
+
+// \u5220\u9664\u914D\u7F6E
+git config --global --unset xxx
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u5206\u652F" tabindex="-1"><a class="header-anchor" href="#\u5206\u652F" aria-hidden="true">#</a> \u5206\u652F</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>// \u5207\u6362\u5206\u652F
+git checkout &lt;branch&gt;
+
+// \u521B\u5EFA\u5206\u652F\u5E76\u5207\u6362\u5230\u8BE5\u5206\u652F
+git checkout -b &lt;branch&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,6),t=[s];function l(c,r){return i(),n("div",null,t)}var o=e(d,[["render",l],["__file","git \u5E38\u7528\u547D\u4EE4.html.vue"]]);export{o as default};
