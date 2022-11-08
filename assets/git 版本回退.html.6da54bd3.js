65ac679c129d34165f8e5c031737fa73d6cf9a78
Vsnoy <59299768+Vsnoy@users.noreply.github.com>
2022-11-08 09:21:39 +0000

    Deploying to gh-pages from  @ 1013b24339c32718be1f92cd04776d3674671fac 🚀

---
+++ assets/git 版本回退.html.6da54bd3.js
@@ -0,0 +1,13 @@
+import{_ as t,o as a,c as n,a as e,b as r,d,e as s,r as l}from"./app.7072a82a.js";const c={},o=d(`<h1 id="git-\u7248\u672C\u56DE\u9000" tabindex="-1"><a class="header-anchor" href="#git-\u7248\u672C\u56DE\u9000" aria-hidden="true">#</a> git \u7248\u672C\u56DE\u9000</h1><h2 id="git-reset" tabindex="-1"><a class="header-anchor" href="#git-reset" aria-hidden="true">#</a> git reset</h2><p>\u56DE\u6EDA\u3002\u5F7B\u5E95\u56DE\u6EDA\u5230\u67D0\u6B21\u63D0\u4EA4\uFF0C\u8BE5\u63D0\u4EA4\u4E4B\u540E\u7684\u6240\u6709\u63D0\u4EA4\u8BB0\u5F55\u90FD\u4F1A\u820D\u5F03\u3002<br> \u4F7F\u7528\u9700\u8981\u8C28\u614E\uFF0C\u4E0D\u5EFA\u8BAE\u5728\u591A\u4EBA\u534F\u4F5C\u65F6\u4F7F\u7528\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>git reset --hard HEAD~n
+
+git reset --hard HEAD^..
+
+git reset --hard &lt;commit&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>git push -f
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="git-revert" tabindex="-1"><a class="header-anchor" href="#git-revert" aria-hidden="true">#</a> git revert</h2><p>\u64A4\u9500\u3002\u4EC5\u9488\u5BF9\u67D0\u6B21\u63D0\u4EA4\u8FDB\u884C\u64A4\u9500\uFF0C\u8BE5\u63D0\u4EA4\u4E4B\u540E\u7684\u6240\u6709\u63D0\u4EA4\u8BB0\u5F55\u4F9D\u7136\u4FDD\u7559\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>git revert HEAD~n
+
+git revert HEAD^..
+
+git revert &lt;commit&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>git push
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="\u53C2\u8003" tabindex="-1"><a class="header-anchor" href="#\u53C2\u8003" aria-hidden="true">#</a> \u53C2\u8003</h2>`,10),v={href:"https://blog.csdn.net/weixin_42412601/article/details/124070871",target:"_blank",rel:"noopener noreferrer"},u=s("git \u7248\u672C\u56DE\u9000");function h(g,m){const i=l("ExternalLinkIcon");return a(),n("div",null,[o,e("ul",null,[e("li",null,[e("a",v,[u,r(i)])])])])}var x=t(c,[["render",h],["__file","git \u7248\u672C\u56DE\u9000.html.vue"]]);export{x as default};
