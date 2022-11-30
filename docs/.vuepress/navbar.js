e2b35189e2ba428a78aef980426cbc73ec4e74ab
Vsnoy <truama@outlook.com>
2022-11-30 13:46:17 +0800

    chore: 🔧

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -1,30 +1,34 @@
 module.exports = [
   {
     text: "碎片",
-    link: encodeURI("/fe-piece/css")
+    link: encodeURI("/fe-piece/css"),
   },
   {
     text: "基础",
-    link: encodeURI("/fe-basic/js/闭包")
+    link: encodeURI("/fe-basic/js/闭包"),
   },
   {
     text: "框架",
-    link: encodeURI("/fe-frame/react/重渲染")
+    link: encodeURI("/fe-frame/react/重渲染"),
   },
   {
     text: "工程",
-    link: encodeURI("/engineer/git/git commit 提交规范")
+    link: encodeURI("/engineer/git/git commit 提交规范"),
   },
   {
     text: "题库",
-    link: encodeURI("/question/闭包")
+    link: encodeURI("/question/闭包"),
   },
-  process.env.NODE_ENV === 'development' && {
+  // process.env.NODE_ENV === 'development' && {
+  //   text: "面试",
+  //   link: encodeURI("/interview/html")
+  // },
+  {
     text: "面试",
-    link: encodeURI("/interview/html")
+    link: encodeURI("/interview/html"),
   },
   {
     text: "资源",
-    link: encodeURI("/resource/study")
+    link: encodeURI("/resource/study"),
   },
-].filter(Boolean)
+].filter(Boolean);
