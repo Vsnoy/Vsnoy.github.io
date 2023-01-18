5839ef4f16307bd1c796bfc9bf4138ef4461d722
Vsnoy <59299768+Vsnoy@users.noreply.github.com>
2023-01-18 03:05:19 +0000

    Deploying to gh-pages from  @ d06de9e90f4dbdefdb7312f5631d650b66bf95bf 🚀

---
+++ assets/gitignore 文件配置.html.35f1c809.js
@@ -0,0 +1,13 @@
+import{_ as e,o as i,c as d,e as n}from"./app.1fe87e6f.js";const t={},s=n(`<h1 id="gitignore-\u6587\u4EF6\u914D\u7F6E" tabindex="-1"><a class="header-anchor" href="#gitignore-\u6587\u4EF6\u914D\u7F6E" aria-hidden="true">#</a> .gitignore \u6587\u4EF6\u914D\u7F6E</h1><p><code>.gitignore</code> \u6587\u4EF6\u7528\u4E8E\u7EDF\u4E00\u914D\u7F6E\u4E0D\u63D0\u4EA4\u5230\u4ED3\u5E93\u4E2D\u7684\u6587\u4EF6\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>.vscode
+
+dist
+node_modules
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="custom-container tip"><p class="custom-container-title">TIP</p><ul><li><p>\u5176\u4F59\u5982\u4E00\u4E9B\u7CFB\u7EDF\u3001\u7F16\u8F91\u5668\u6587\u4EF6\u4E4B\u7C7B\u7684\u914D\u7F6E\uFF0C\u53EF\u501F\u52A9 <code>vscode</code> \u63D2\u4EF6 <code>gitignore</code> \u751F\u6210\u3002</p></li><li><p><code>.gitignore</code> \u6587\u4EF6\u53EA\u80FD\u5FFD\u7565\u90A3\u4E9B\u539F\u6765\u6CA1\u6709\u88AB <code>track</code> \u7684\u6587\u4EF6\uFF0C\u5982\u679C\u67D0\u4E9B\u6587\u4EF6\u5DF2\u7ECF\u88AB\u7EB3\u5165\u4E86\u7248\u672C\u7BA1\u7406\u4E2D\uFF0C\u5219\u4FEE\u6539 <code>.gitignore</code> \u6587\u4EF6\u662F\u65E0\u6548\u7684\u3002\u89E3\u51B3\u65B9\u6CD5\u5982\u4E0B\uFF1A</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>// \u9488\u5BF9\u5355\u4E2A\u6587\u4EF6
+git rm --cached &lt;file&gt;
+git add &lt;file&gt;
+git commit -m &#39;update .gitignore&#39;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>// \u9488\u5BF9\u6240\u6709\u6587\u4EF6
+git rm -r --cached .
+git add .
+git commit -m &#39;update .gitignore&#39;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li></ul></div>`,4),c=[s];function a(l,r){return i(),d("div",null,c)}var v=e(t,[["render",a],["__file","gitignore \u6587\u4EF6\u914D\u7F6E.html.vue"]]);export{v as default};
---
+++ assets/引用类型地址.html.3fa69251.js
@@ -0,0 +1,49 @@
+import{_ as r,o as t,c,a as n,d as e,e as a,b as i,r as d}from"./app.1fe87e6f.js";const v={},o=a(`<h1 id="\u5F15\u7528\u7C7B\u578B\u5730\u5740" tabindex="-1"><a class="header-anchor" href="#\u5F15\u7528\u7C7B\u578B\u5730\u5740" aria-hidden="true">#</a> \u5F15\u7528\u7C7B\u578B\u5730\u5740</h1><div class="custom-container tip"><p class="custom-container-title">TIP</p><p>\u5F15\u7528\u7C7B\u578B\u53D8\u91CF\uFF0C\u5B58\u50A8\u7684\u90FD\u662F\u8BE5\u5F15\u7528\u7C7B\u578B\u6240\u5728\u7684\u5730\u5740\u3002</p></div><h2 id="\u4E60\u9898\u4E00" tabindex="-1"><a class="header-anchor" href="#\u4E60\u9898\u4E00" aria-hidden="true">#</a> \u4E60\u9898\u4E00</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>
+let arr = [1, 2, 3] // arr --&gt; 0x001 --&gt; [1, 2, 3]
+
+for (let x of arr) { // arr --&gt; 0x001
+  arr = [4, 5, 6] // arr --&gt; 0x002 --&gt; [4, 5, 6]
+  console.log(x)
+}
+
+// \u8F93\u51FA
+1
+2
+3
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),u={id:"\u4E60\u9898\u4E8C",tabindex:"-1"},b=n("a",{class:"header-anchor",href:"#\u4E60\u9898\u4E8C","aria-hidden":"true"},"#",-1),m=i(" \u4E60\u9898\u4E8C "),_=a(`<div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>var a = { n: 1 }
+var b = a
+a.x = a = { n: 2 }
+
+console.log(a.x)
+console.log(b.x)
+
+// \u8F93\u51FA
+undefined
+{ n: 2 }
+
+// \u89E3\u6790
+\u6D89\u53CA\u77E5\u8BC6\u70B9\uFF1A\u8FD0\u7B97\u7B26\u4F18\u5148\u7EA7\u3001\u5F15\u7528\u7C7B\u578B\u5730\u5740\u3001\u8D4B\u503C\u987A\u5E8F\u3002
+
+\u9010\u884C\u89E3\u6790\uFF1A
+a = { n: 1 }
+a --&gt; 0x001 --&gt; { n: 1 }
+
+b = a
+b --&gt; 0x001 --&gt; { n: 1 }
+
+a.x = a = { n: 2 }
+\u70B9\u53F7\u8FD0\u7B97\u7B26\u4F18\u5148\u7EA7\u5927\u4E8E\u8D4B\u503C\u8FD0\u7B97\u7B26\uFF0C\u5219\u5148\u6267\u884C\u70B9\u53F7\u8FD0\u7B97
+a.x
+[0x001].x --&gt; { n: 1, x: undefined }
+0x001 --&gt; { n: 1, x: undefined }
+\u540E\u81EA\u53F3\u5411\u5DE6\u6267\u884C\u8D4B\u503C\u8FD0\u7B97
+a = { n: 2 }
+a --&gt; 0x002 --&gt; { n: 2 }
+a.x = a
+[0x001].x = { n: 2 } --&gt; { n:1, x: {n: 2 } }
+0x001 --&gt; { n:1, x: { n: 2 } }
+
+\u53EF\u4EE5\u770B\u5230\uFF0C\u5F53\u524D
+a --&gt; 0x002 --&gt; { n: 2 }
+b --&gt; 0x001 --&gt; { n:1, x: { n: 2 } }
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),h={href:"https://github.com/zhuzhh/blog/issues/7",target:"_blank",rel:"noopener noreferrer"},x=i("\u89E3\u6790 js \u8FDE\u7EED\u8D4B\u503C\u7684\u5751"),g=n("br",null,null,-1),p={href:"https://cloud.tencent.com/developer/article/1093667",target:"_blank",rel:"noopener noreferrer"},f=i("\u641E\u61C2 JavaScript \u4E2D\u7684\u8FDE\u7EED\u8D4B\u503C"),k=n("br",null,null,-1),B={href:"https://juejin.cn/post/7069358111881953317",target:"_blank",rel:"noopener noreferrer"},N=i("\u5173\u4E8E a.x = a = { n: 2 } \u9762\u8BD5\u9898\u89E3\u6790");function V(j,E){const l=d("Badge"),s=d("ExternalLinkIcon");return t(),c("div",null,[o,n("h2",u,[b,m,e(l,{text:"hard"})]),_,n("p",null,[n("a",h,[x,e(s)]),g,n("a",p,[f,e(s)]),k,n("a",B,[N,e(s)])])])}var z=r(v,[["render",V],["__file","\u5F15\u7528\u7C7B\u578B\u5730\u5740.html.vue"]]);export{z as default};
