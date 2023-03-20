100f6646b2f1b3346b7f7578136d6895b82dea63
Vsnoy <59299768+Vsnoy@users.noreply.github.com>
2023-03-20 14:12:34 +0000

    Deploying to gh-pages from  @ 5b4d8bbf37a4a04a494812779846976c2c99b421 🚀

---
+++ assets/水平垂直居中.html.445bb921.js
@@ -0,0 +1,173 @@
+import{_ as i,o as n,c as e,e as d}from"./app.d29afbf8.js";const l={},s=d(`<h1 id="\u6C34\u5E73\u5782\u76F4\u5C45\u4E2D" tabindex="-1"><a class="header-anchor" href="#\u6C34\u5E73\u5782\u76F4\u5C45\u4E2D" aria-hidden="true">#</a> \u6C34\u5E73\u5782\u76F4\u5C45\u4E2D</h1><h2 id="\u7EDD\u5BF9\u5B9A\u4F4D" tabindex="-1"><a class="header-anchor" href="#\u7EDD\u5BF9\u5B9A\u4F4D" aria-hidden="true">#</a> \u7EDD\u5BF9\u5B9A\u4F4D</h2><h3 id="\u7EDD\u5BF9\u5B9A\u4F4D-calc" tabindex="-1"><a class="header-anchor" href="#\u7EDD\u5BF9\u5B9A\u4F4D-calc" aria-hidden="true">#</a> \u7EDD\u5BF9\u5B9A\u4F4D + calc</h3><p>\u5B50\u5143\u7D20\u5B9A\u5BBD\u9AD8\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>&lt;style&gt;
+  #parent {
+    width: 300px;
+    height: 300px;
+    background: #ccc;
+    position: relative;
+  }
+
+  #child {
+    width: 100px;
+    height: 100px;
+    background: #c9394a;
+    position: absolute;
+    left: calc(50% - 50px);
+    top: calc(50% - 50px);
+  }
+&lt;/style&gt;
+
+&lt;div id=&quot;parent&quot;&gt;
+  &lt;div id=&quot;child&quot;&gt;&lt;/div&gt;
+&lt;/div&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="\u7EDD\u5BF9\u5B9A\u4F4D-\u8D1F-margin" tabindex="-1"><a class="header-anchor" href="#\u7EDD\u5BF9\u5B9A\u4F4D-\u8D1F-margin" aria-hidden="true">#</a> \u7EDD\u5BF9\u5B9A\u4F4D + \u8D1F margin</h3><p>\u5B50\u5143\u7D20\u5B9A\u5BBD\u9AD8\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>&lt;style&gt;
+  #parent {
+    width: 300px;
+    height: 300px;
+    background: #ccc;
+    position: relative;
+  }
+
+  #child {
+    width: 100px;
+    height: 100px;
+    background: #c9394a;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    margin-left: -50px;
+    margin-right: -50px;
+  }
+&lt;/style&gt;
+
+&lt;div id=&quot;parent&quot;&gt;
+  &lt;div id=&quot;child&quot;&gt;&lt;/div&gt;
+&lt;/div&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="\u7EDD\u5BF9\u5B9A\u4F4D-transform" tabindex="-1"><a class="header-anchor" href="#\u7EDD\u5BF9\u5B9A\u4F4D-transform" aria-hidden="true">#</a> \u7EDD\u5BF9\u5B9A\u4F4D + transform</h3><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>&lt;style&gt;
+  #parent {
+    width: 300px;
+    height: 300px;
+    background: #ccc;
+    position: relative;
+  }
+
+  #child {
+    width: 100px;
+    height: 100px;
+    background: #c9394a;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+&lt;/style&gt;
+
+&lt;div id=&quot;parent&quot;&gt;
+  &lt;div id=&quot;child&quot;&gt;&lt;/div&gt;
+&lt;/div&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="\u7EDD\u5BF9\u5B9A\u4F4D-\u4E0A\u4E0B\u5DE6\u53F3-0-margin-auto" tabindex="-1"><a class="header-anchor" href="#\u7EDD\u5BF9\u5B9A\u4F4D-\u4E0A\u4E0B\u5DE6\u53F3-0-margin-auto" aria-hidden="true">#</a> \u7EDD\u5BF9\u5B9A\u4F4D + \u4E0A\u4E0B\u5DE6\u53F3 0 + margin: auto</h3><p>\u5B50\u5143\u7D20\u5B9A\u5BBD\u9AD8\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>&lt;style&gt;
+  #parent {
+    width: 300px;
+    height: 300px;
+    background: #ccc;
+    position: relative;
+  }
+
+  #child {
+    width: 100px;
+    height: 100px;
+    background: #c9394a;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+  }
+&lt;/style&gt;
+
+&lt;div id=&quot;parent&quot;&gt;
+  &lt;div id=&quot;child&quot;&gt;&lt;/div&gt;
+&lt;/div&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="flex" tabindex="-1"><a class="header-anchor" href="#flex" aria-hidden="true">#</a> Flex</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>&lt;style&gt;
+  #parent {
+    width: 300px;
+    height: 300px;
+    background: #ccc;
+  
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  
+    /**
+    flex-wrap: wrap;
+    align-content: center;
+    **/
+  }
+  
+  #child {
+    width: 100px;
+    height: 100px;
+    background: #c9394a;
+  
+    /*align-self: center;*/
+  }
+&lt;/style&gt;
+
+&lt;div id=&quot;parent&quot;&gt;
+  &lt;div id=&quot;child&quot;&gt;&lt;/div&gt;
+&lt;/div&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>&lt;style&gt;
+  #parent {
+    width: 300px;
+    height: 300px;
+    background: #ccc;
+  
+    display: flex;
+  }
+  
+  #child {
+    width: 100px;
+    height: 100px;
+    background: #c9394a;
+  
+    margin: auto;
+  }
+&lt;/style&gt;
+
+&lt;div id=&quot;parent&quot;&gt;
+  &lt;div id=&quot;child&quot;&gt;&lt;/div&gt;
+&lt;/div&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="grid" tabindex="-1"><a class="header-anchor" href="#grid" aria-hidden="true">#</a> Grid</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>&lt;style&gt;
+  #parent {
+    width: 300px;
+    height: 300px;
+    background: #ccc;
+  
+    display: grid;
+  
+    /*\u4EE5\u4E0B\u4E24\u7EC4\u6392\u5217\u7EC4\u5408\u5747\u53EF*/
+  
+    /** 
+    justify-content: center;
+    justify-items: center;
+  
+    align-content: center;
+    align-items: center;
+    **/
+  }
+  
+  #child {
+    width: 100px;
+    height: 100px;
+    background: #c9394a;
+  
+    justify-self: center;
+    align-self: center;
+  }
+}
+&lt;/style&gt;
+
+&lt;div id=&quot;parent&quot;&gt;
+  &lt;div id=&quot;child&quot;&gt;&lt;/div&gt;
+&lt;/div&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,18),v=[s];function a(r,t){return n(),e("div",null,v)}var u=i(l,[["render",a],["__file","\u6C34\u5E73\u5782\u76F4\u5C45\u4E2D.html.vue"]]);export{u as default};
