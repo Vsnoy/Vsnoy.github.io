ed6afc9b7d27740e3b083c270d570a48cc75df72
Vsnoy <59299768+Vsnoy@users.noreply.github.com>
2023-05-20 13:06:34 +0000

    Deploying to gh-pages from  @ 628bd0e9db64c3887e2dc778d0bdde8d1c40266a 🚀

---
+++ assets/linux 常用命令.html.2e191a89.js
@@ -0,0 +1,30 @@
+import{_ as n,o as i,c as e,d}from"./app.9a4099eb.js";const s={},l=d(`<h1 id="linux-\u5E38\u7528\u547D\u4EE4" tabindex="-1"><a class="header-anchor" href="#linux-\u5E38\u7528\u547D\u4EE4" aria-hidden="true">#</a> linux \u5E38\u7528\u547D\u4EE4</h1><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>// \u5207\u6362\u76EE\u5F55
+cd xxx
+
+// \u67E5\u770B\u5F53\u524D\u76EE\u5F55\u4E0B\u6587\u4EF6
+ls
+
+// \u67E5\u770B\u5F53\u524D\u76EE\u5F55\u8DEF\u5F84
+pwd
+
+// \u67E5\u770B\u5F53\u524D\u6587\u4EF6\u5185\u5BB9
+cat xxx
+
+// \u4FEE\u6539\u6587\u4EF6\u3001\u6587\u4EF6\u5939\u540D\uFF0C\u6216\u79FB\u52A8\u6587\u4EF6
+mv xxx xxx
+
+// \u590D\u5236\u6587\u4EF6
+cp xxx xxx_copy
+
+// \u65B0\u5EFA\u6587\u4EF6
+touch xxx
+
+// \u65B0\u5EFA\u6587\u4EF6\u5939
+mkdir xxx
+
+// \u5220\u9664\u6587\u4EF6
+rm xxx
+
+// \u5220\u9664\u6587\u4EF6\u5939\u53CA\u5176\u4E0B\u6240\u6709\u6587\u4EF6
+rm -rf xxx
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),v=[l];function a(c,r){return i(),e("div",null,v)}var x=n(s,[["render",a],["__file","linux \u5E38\u7528\u547D\u4EE4.html.vue"]]);export{x as default};
