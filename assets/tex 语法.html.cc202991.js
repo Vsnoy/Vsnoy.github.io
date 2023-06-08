31af5ad0323881e2fc33fcddac7f58ac88064613
Vsnoy <59299768+Vsnoy@users.noreply.github.com>
2023-06-08 02:23:20 +0000

    Deploying to gh-pages from  @ ce2e7c0fbcfe33c11e1b0ab2249586360bda153a 🚀

--- assets/tex 语法.html.cc202991.js
+++
@@ -1,9 +0,0 @@
-import{_ as e,o as s,c as n,d}from"./app.dcbbd10f.js";const i={},a=d(`<h1 id="tex-\u8BED\u6CD5" tabindex="-1"><a class="header-anchor" href="#tex-\u8BED\u6CD5" aria-hidden="true">#</a> Tex \u8BED\u6CD5</h1><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>- \u5206\u53F7\uFF1A\\dfrac{}{}
-- \u52A0\u51CF\u53F7\uFF1A\\pm | \\mp
-- \u7701\u7565\u53F7\uFF1A\\dots | \\cdots | \\vdots | \\ddots
-- \u6839\u53F7\uFF1A\\sqrt{} | \\sqrt[]{}
-- \u5206\u7EC4\u5927\u62EC\u53F7\uFF1A\\begin{cases} \\end{cases}
-- \u5BF9\u6570\uFF1A\\log_{}{} | \\ln{} | \\lg{}
-- \u4E0A\u4E0B\u89D2\u6807\uFF1A^{} | _{}
-- \u7A7A\u683C\uFF1A\\space | \\quad
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),t=[a];function c(l,r){return s(),n("div",null,t)}var v=e(i,[["render",c],["__file","tex \u8BED\u6CD5.html.vue"]]);export{v as default};
